5d596d68471603d76dabbfbd3b00c5803969dffc
Ayush dixit <63745645+minecraftdixit@users.noreply.github.com>
2024-02-04 17:21:49 +0530

    Update cb_tb.v

--- lab3/src/cb_tb.v
+++ lab3/src/cb_tb.v
@@ -1,15 +1,15 @@
 module cb_tb;
-    reg [7:0] num;
+    reg [31:0] num;
     wire [63:0] sum;
 
     cb #(10000) u1 (.num(num), .sum(sum));
 
     initial begin
-         num = 8'd2; 
+         num = 31'd2; 
 
         #10;
 
-         $display("Sum = %d", sum);
+         $monitor("Sum = %d , num = %d", sum  , num );
 
          $finish;
     end
