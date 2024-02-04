ed764ca5f0b181a298c46edc5a9766a0dca14596
Ayush dixit <63745645+minecraftdixit@users.noreply.github.com>
2024-02-04 17:20:58 +0530

    Update cb.v

--- lab3/src/cb.v
+++ lab3/src/cb.v
@@ -1,12 +1,17 @@
+module cb #(parameter q = 10000)(
+    input [31:0] num,
+    output reg [63:0] sum
+);
 
-//module repeated sum 
-module cb #(parameter q = 10000)(input [7:0] num, output reg [63:0] sum);
     integer i;
+    reg [31:0] rand_num;
+
     always @* begin
         sum = 0;
-        for (i = 0; i < q; i = i + 1) begin
-            sum = sum + num;
+        for (i = 0; i < num; i = i + 1) begin
+            rand_num = $urandom_range(90000 - 10000 + 1) + 10000;  
+            sum = sum + rand_num;
         end
     end
-endmodule
 
+endmodule
